b33f02e3b51168a2edaade8622b99edb5bc28913
MJ Rossetti <datacreativellc@gmail.com>
2017-04-06 14:05:22 -0400

    Replace placeholder data with dynamic data from the server

--- routes/robots.js
+++ routes/robots.js
@@ -1,41 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
-var myRobots = [
-  {
-    "_id":"58e487ad32ac2000f07acc4c",
-    "updated_at":"2017-04-05T05:59:09.694Z",
-    "created_at":"2017-04-05T05:59:09.694Z",
-    "name":"r2d2",
-    "description":"holds a secret message",
-    "in_stock":100,
-    "__v":0
-  },
-  {
-    "_id":"58e487ad32ac2000f07acc4d",
-    "updated_at":"2017-04-05T05:59:09.696Z",
-    "created_at":"2017-04-05T05:59:09.696Z",
-    "name":"bb8",
-    "description":"rolls around",
-    "in_stock":75,
-    "__v":0
-  },
-  {
-    "_id":"58e487ad32ac2000f07acc4b",
-    "updated_at":"2017-04-05T05:59:09.687Z",
-    "created_at":"2017-04-05T05:59:09.687Z",
-    "name":"c3po",
-    "description":"specializes in language translation",
-    "in_stock":50,
-    "__v":0
-  }
-] // use example data
+var fetch = require('node-fetch');
 
 /* List Robots */
 
 router.get('/robots', function(req, res, next) {
-  console.log("LISTING ROBOTS", myRobots)
-  res.render('robots/index', {robots: myRobots, title: "All Robots"});
+  var url = "https://southernct-443-robots-api.herokuapp.com/api/robots.json"
+
+  fetch(url)
+    .then(function(response) {
+      response.json()
+        .then(function(json){
+          console.log("LISTING ROBOTS", json)
+          res.render('robots/index', {robots: json, title: "All Robots"});
+        })
+    })
+    .catch(function(err){
+      console.log("GOT AN ERROR:", err)
+      res.send({error: `OOPS - SERVER ERROR ${err}`});
+    })
 });
 
 /* Show Robot */
@@ -44,14 +27,21 @@ router.get('/robots/:id', function(req, res, next) {
   var robotId = req.params.id;
   var robot = myRobots.find(function(robot){ return robot._id == robotId }) // use static hard-coded data for now
 
-  if (robot) {
-    console.log("SHOWING ROBOT", robot)
-    res.render('robots/show', {robot: robot, title: `Robot ${robotId}`});
-  } else {
-    var errorMessage = `OOPS - COULDN'T FIND ROBOT ${robotId}`
-    console.log(errorMessage)
-    res.send(errorMessage)
-  }
+  var url = `https://southernct-443-robots-api.herokuapp.com/api/robots/${robotId}.json`
+
+  fetch(url)
+    .then(function(response) {
+      response.json()
+        .then(function(json){
+          console.log("SHOWING ROBOT", json)
+          res.render('robots/show', {robot: json, title: `Robot ${robotId}`});
+        })
+    })
+    .catch(function(err){
+      var errorMessage = `OOPS - COULDN'T FIND ROBOT ${robotId}`
+      console.log(errorMessage)
+      res.send(errorMessage)
+    })
 });
 
 module.exports = router;
